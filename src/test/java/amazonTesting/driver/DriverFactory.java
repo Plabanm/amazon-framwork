a338bed741877f057877c99512c2d119bf1bddae
plabanm <plabanm.2@gmail.com>
2019-07-14 02:05:28 +0100

    Driver Factory Update

--- src/test/java/amazonTesting/driver/DriverFactory.java
+++ src/test/java/amazonTesting/driver/DriverFactory.java
@@ -1,18 +1,13 @@
 package amazonTesting.driver;
 
-import com.sun.glass.ui.View;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.PageFactory;
 
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 public class DriverFactory {
@@ -32,9 +27,7 @@ public class DriverFactory {
         switch (browser){
             case "chrome":
                 WebDriverManager.chromedriver().setup();
-                DesiredCapabilities cap = new DesiredCapabilities();
-                cap.setBrowserName("chrome");
-                driver = new RemoteWebDriver(new URL("http://192.168.0.43:4444/wd/hub"), cap);
+                driver = new ChromeDriver();
                 driver.get("https://www.amazon.co.uk/");
                 break;
             case "ie":
